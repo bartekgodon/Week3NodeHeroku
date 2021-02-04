6e3265a1b4f26bb77a8a117d5c2940d214df4dd3
bartek.godon <bartek.godon@dai.co.uk>
2021-02-04 16:18:16 +0000

    Added more meaningful message

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var adminRouter = express.Router();
 // route middleware that will happen on every request
 adminRouter.use(function(req, res, next) {
     // log each request to the console
-    console.log(req.method, req.url);
+    console.log( "Here is my log - "+ req.method + req.url);
     // continue doing what we were doing and go to the route
     next(); 
 });   
