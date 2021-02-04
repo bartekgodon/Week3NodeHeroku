b6b651c08fc65afc9269ec0e648215d772f83052
bartek.godon <bartek.godon@dai.co.uk>
2021-02-04 15:13:42 +0000

    Added package.json and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.end('Hello Word\n');
+}).listen(8080);
+
+console.log('Server running at localhost:8080');
