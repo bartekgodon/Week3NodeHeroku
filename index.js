555867e7ed5f7da6ebb4aa5ff8ccfe6b739c7adc
bartek.godon <bartek.godon@dai.co.uk>
2021-02-04 15:47:16 +0000

    Added more stuff

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var http = require("http");
-
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-ALlow-Origin': '*'
+    });
 
-    response.end('Hello Word\n');
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+    
+    readStream.pipe(response);
+    
 }).listen(PORT);
 
 console.log(`Server running at port ${PORT}`);
