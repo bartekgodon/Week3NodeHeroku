99d0e5c38adcff7163b11fa81f73337f719cea0b
bartek.godon <bartek.godon@dai.co.uk>
2021-02-04 15:34:21 +0000

    Change index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     response.end('Hello Word\n');
-}).listen(8080);
+}).listen(PORT);
 
-console.log('Server running at localhost:8080');
+console.log(`Server running at port ${PORT}`);
